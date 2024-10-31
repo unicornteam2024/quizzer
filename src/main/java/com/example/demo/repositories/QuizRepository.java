f9f73af8c0957ce2900e297e723a55321327b16c
Temitope Ajayi <bhh500@myy.haaga-helia.fi>
2024-11-01 01:13:48 +0200

    Added and Update QuizRepository.java

--- src/main/java/com/example/demo/repositories/QuizRepository.java
+++ src/main/java/com/example/demo/repositories/QuizRepository.java
@@ -4,7 +4,7 @@ import com.example.demo.entities.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    
+     
 }
 
 
