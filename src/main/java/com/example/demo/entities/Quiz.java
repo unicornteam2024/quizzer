0a37838c99cb8545dd82a27dfba2613f7163c648
Hardik <hardiksavasni2828@address.com>
2024-11-01 15:22:26 +0200

    import
    import

--- src/main/java/com/example/demo/entities/Quiz.java
+++ src/main/java/com/example/demo/entities/Quiz.java
@@ -1,7 +1,13 @@
 package com.example.demo.entities;
  
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 
 @Entity
 public class Quiz {
